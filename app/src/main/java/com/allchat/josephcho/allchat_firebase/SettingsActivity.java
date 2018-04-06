30ed202ada4ca1dafc6af627f1b34bfc55199cab
Joseph Cho <cho.joseph17@gmail.com>
2018-04-06 18:05:56 -0400

    account setting page now pulls user data from the Firebase db

--- app/src/main/java/com/allchat/josephcho/allchat_firebase/SettingsActivity.java
+++ app/src/main/java/com/allchat/josephcho/allchat_firebase/SettingsActivity.java
@@ -2,12 +2,61 @@ package com.allchat.josephcho.allchat_firebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private DatabaseReference mUserDB;
+    private FirebaseUser mCurrentUser;
+
+    private CircleImageView mProfilePic;
+    private TextView mName;
+    private TextView mStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        mProfilePic = (CircleImageView)findViewById(R.id.settings_image);
+        mName = (TextView)findViewById(R.id.settings_name);
+        mStatus = (TextView)findViewById(R.id.settings_status);
+
+        mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+        String currentUserId = mCurrentUser.getUid();
+
+        mUserDB = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserId);
+        mUserDB.addValueEventListener(new ValueEventListener() {
+            @Override
+            //handles pulling user data from the db and populating it into the app
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String name = dataSnapshot.child("name").getValue().toString();
+                String image = dataSnapshot.child("image").getValue().toString();
+                String status = dataSnapshot.child("status").getValue().toString();
+                String thumbnail = dataSnapshot.child("thumbnail").getValue().toString();
+
+                mName.setText(name);
+                mStatus.setText(status);
+
+
+            }
+
+            @Override
+            //handles
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
